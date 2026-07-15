e8713f7c217b7eb6da2970ffbd2357832af355c7
Vishal-M1205 <vishalmanivannan1205@gmail.com>
2026-07-15 09:59:13 +0530

    First

---
+++ scripts/index.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+  
+    $('.house-carousel').slick({
+        infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  responsive:[
+    {
+        breakpoint: 1024,
+        settings:{
+            slidesToShow:3,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 998,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 580,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1,
+            arrows:false,
+            dots:true
+        }
+    },
+  ]
+    })
+
+    $('.retirement-carousel').slick({
+        infinite:true,
+        speed:300,
+        responsive:[
+    {
+        breakpoint: 1024,
+        settings:{
+            slidesToShow:2,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 998,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 580,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1,
+            arrows:false,
+            dots:true
+        }
+    },
+  ]
+    })
+    $('.resident-carousel').slick({
+        infinite:true,
+        speed:300,
+        arrows:false,
+            dots:true,
+        responsive:[
+    {
+        breakpoint: 1024,
+        settings:{
+            slidesToShow:2,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 998,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 580,
+        settings:{
+            slidesToShow:1,
+            slidesToScroll: 1,
+            
+        }
+    },
+  ]
+    })
+
+})
